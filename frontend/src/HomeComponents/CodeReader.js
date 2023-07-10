a5cb0d29750569bfd2266a4260eedcacef2bf402
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-07-10 12:02:12 +0100

    barcode reader and fetch

--- frontend/src/HomeComponents/CodeReader.js
+++ frontend/src/HomeComponents/CodeReader.js
@@ -1,18 +1,32 @@
 import { Html5QrcodeScanner } from "html5-qrcode";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const CodeReader = ()=> {
+
+    const [barcode, setBarcode] = useState([])
+    const [fetchedBook, setBook] = useState({})
     
     useEffect(()=> {  function onScanSuccess(decodedText, decodedResult) {
-        console.log(`Code scanned = ${decodedText}`, decodedResult)}
-         
-        var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 5, qrbox: 400 });
+        console.log(`Code scanned = ${decodedText}`, decodedResult)
+        if(decodedText) {setBarcode(decodedText);}}
+        var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 5, qrbox: 300 });
         html5QrcodeScanner.render(onScanSuccess);},[])
+
+    useEffect(()=>{
+        console.log("barcode changed")
+        fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:" + barcode)
+        .then(res => res.json())
+        .then(res=> setBook(res))
+
+    },[barcode])
+    
 
     return(
         <>        
             <div id='qr-reader' style={{width: 50 + 'vw'}}/>
-            <div id='show-book'></div>
+            <div id='book-details'>
+                {barcode}
+            </div>
         </>
 
     )
